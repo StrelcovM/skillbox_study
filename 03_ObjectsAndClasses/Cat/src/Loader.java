ef9a6f3a9b84b267f21f927b482a03cf0054718f
Strelkov Maxim <stremaks@icloud.com>
2020-07-04 15:08:42 +0300

    Cats created and feeded and meowed

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -3,8 +3,35 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        Cat cat = new Cat();
+        Cat tima = new Cat();
+        Cat oleg = new Cat();
+        Cat vasya = new Cat();
+        Cat anton = new Cat();
+        Cat murka = new Cat();
 
-        System.out.println(cat.getStatus());
+        System.out.println("Tima weight: " + tima.getWeight());
+        System.out.println("Oleg weight: " + oleg.getWeight());
+        System.out.println("Vasya weight: " + vasya.getWeight());
+        System.out.println("Anton weight: " + anton.getWeight());
+        System.out.println("Murka weight: " + murka.getWeight());
+
+        murka.feed(3.0);
+        tima.feed(1.0);
+
+        System.out.println();
+
+        System.out.println("Murka weight: " + murka.getWeight());
+        System.out.println("Tima weight: " + tima.getWeight());
+
+        while (!murka.getStatus().equals("Exploded"))
+            murka.feed(1.0);
+
+        System.out.println("\nMurka status: " + murka.getStatus());
+
+        while (!tima.getStatus().equals("Dead"))
+            tima.meow();
+
+        System.out.println("\nTima status: " + tima.getStatus());
+
     }
 }
