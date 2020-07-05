e1c338b69e12d22a36e8edfcbf2d5e7a0347e0ff
Strelkov Maxim <stremaks@icloud.com>
2020-07-05 23:02:42 +0300

    Homework 4.4.2 done

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -1,10 +1,21 @@
 
-public class Loader
-{
-    public static void main(String[] args)
-    {
+public class Loader {
+    public static void main(String[] args) {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        int sum = 0;
 
-        System.out.println(text);
+        for (int i = 0; i < text.length(); i++) {
+            String salary = "";
+
+            while (Character.isDigit(text.charAt(i))) {
+                salary += text.charAt(i);
+                i++;
+            }
+
+            if (salary.length() > 0)
+                sum += Integer.parseInt(salary);
+            salary = "";
+        }
+        System.out.println(sum);
     }
 }
