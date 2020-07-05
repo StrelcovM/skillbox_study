80ff56cb7159b009329959b212adea05c1b8e5d8
Strelkov Maxim <stremaks@icloud.com>
2020-07-05 23:23:42 +0300

    Homework 4.4.* done

--- 04_NumbersStringsAndDates/Placeholder/src/Main.java
+++ 04_NumbersStringsAndDates/Placeholder/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        System.out.println(searchAndReplaceDiamonds(input, "***"));
+
+    }
+    private static String searchAndReplaceDiamonds(String text, String placeholder){
+        String result = "";
+        result = text.substring(0, text.indexOf("<")) + placeholder + text.substring(text.indexOf(">") + 1);
+        return result;
     }
 }
