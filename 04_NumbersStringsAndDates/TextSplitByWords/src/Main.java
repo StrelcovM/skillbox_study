8a9999b15be1c0c3fb9a458c71ec83a56251d058
Strelkov Maxim <stremaks@icloud.com>
2020-07-06 00:15:46 +0300

    HomeWork 4.5.2 done

--- 04_NumbersStringsAndDates/TextSplitByWords/src/Main.java
+++ 04_NumbersStringsAndDates/TextSplitByWords/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+
+        String[] words = text.split("\\W");
+
+        for (String s : words) {
+            if (!s.equals(""))
+                System.out.println(s);
+        }
     }
 }
