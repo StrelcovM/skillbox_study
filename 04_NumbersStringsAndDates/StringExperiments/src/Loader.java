94ff2af6266166a3c6a692a661ef952db84674c3
Strelkov Maxim <stremaks@icloud.com>
2020-07-05 23:58:14 +0300

    Homework 4.5.1 done

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -1,21 +1,15 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Loader {
     public static void main(String[] args) {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
         int sum = 0;
+        Pattern p = Pattern.compile("\\d+\\D");
+        Matcher m = p.matcher(text);
 
-        for (int i = 0; i < text.length(); i++) {
-            String salary = "";
-
-            while (Character.isDigit(text.charAt(i))) {
-                salary += text.charAt(i);
-                i++;
-            }
-
-            if (salary.length() > 0)
-                sum += Integer.parseInt(salary);
-            salary = "";
-        }
+        while (m.find())
+            sum += Integer.parseInt(text.substring(m.start(), m.end() - 1));
         System.out.println(sum);
     }
 }
