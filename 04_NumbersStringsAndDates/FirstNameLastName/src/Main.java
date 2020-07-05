490cf38b83fc703c8f8a895847ba8f688adc4eef
Strelkov Maxim <stremaks@icloud.com>
2020-07-05 23:14:06 +0300

    Homework 4.4.3 done

--- 04_NumbersStringsAndDates/FirstNameLastName/src/Main.java
+++ 04_NumbersStringsAndDates/FirstNameLastName/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        System.out.println("Фамилия: " + name.substring(0, name.indexOf(" ")));
+        System.out.println("Имя: " + name.substring(name.indexOf(" ") + 1, name.lastIndexOf(" ")));
+        System.out.println("Отчество: " + name.substring(name.lastIndexOf(" ") + 1));
     }
 }
