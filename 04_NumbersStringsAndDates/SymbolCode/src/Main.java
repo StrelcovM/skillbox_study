17e6b52cf04174c67f66e26ee1310b45350208dc
Strelkov Maxim <stremaks@icloud.com>
2020-07-05 22:42:24 +0300

    Homework 4.4.1 done

--- 04_NumbersStringsAndDates/SymbolCode/src/Main.java
+++ 04_NumbersStringsAndDates/SymbolCode/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        
+        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String alphabetRus = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        for (int i = 0; i < alphabet.length(); i++) {
+            System.out.println(alphabet.charAt(i) + ": " + String.format("%04X", (int) alphabet.charAt(i)));
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < alphabetRus.length(); i++){
+            System.out.println(alphabetRus.charAt(i) + ": " + String.format("%04X", (int)alphabetRus.charAt(i)));
+        }
     }
 }
