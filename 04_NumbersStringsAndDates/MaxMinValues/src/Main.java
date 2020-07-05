3922eef94e2f7f104c046cc535baa38c43a0a3b3
Strelkov Maxim <stremaks@icloud.com>
2020-07-05 18:11:15 +0300

    HomeWork 4.2

--- 04_NumbersStringsAndDates/MaxMinValues/src/Main.java
+++ 04_NumbersStringsAndDates/MaxMinValues/src/Main.java
@@ -1,3 +1,20 @@
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("Max_byte: " + Byte.MAX_VALUE);
+        System.out.println("Min_byte: " + Byte.MIN_VALUE);
+        System.out.println("Max_short: " + Short.MAX_VALUE);
+        System.out.println("Min_short: " + Short.MIN_VALUE);
+        System.out.println("Max_int: " + Integer.MAX_VALUE);
+        System.out.println("Min_int: " + Integer.MIN_VALUE);
+        System.out.println("Max_long: " + Long.MAX_VALUE);
+        System.out.println("Min_long: " + Long.MIN_VALUE);
+        System.out.println("Max_float: " + Float.MAX_VALUE);
+        System.out.println("Min_float: " + Float.MIN_VALUE);
+        System.out.println("Max_double: " + Double.MAX_VALUE);
+        System.out.println("Min_double: " + Double.MIN_VALUE);
+        System.out.println("Max_character: " + Character.MAX_VALUE);
+        System.out.println("Min_character: " + Character.MIN_VALUE);
+        System.out.println("True_boolean: " + Boolean.TRUE);
+        System.out.println("False_boolean: " + Boolean.FALSE);
+    }
 }
