c24739102477ccdbcd0db88990799b9f0cdc0a59
Strelkov Maxim <stremaks@icloud.com>
2020-07-05 11:17:17 +0300

    class container corrected and method sumDigits is implemented

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -3,13 +3,21 @@ public class Main
     public static void main(String[] args)
     {
         Container container = new Container();
+        Main main = new Main();
         container.count += 7843;
-
+        System.out.println(main.sumDigits(12345));
+        System.out.println(main.sumDigits(10));
+        System.out.println(main.sumDigits(5059191));
     }
 
     public Integer sumDigits(Integer number)
     {
         //@TODO: write code here
-        return 0;
+        Integer res = 0;
+        String num = number.toString();
+        for(int  i = 0; i < num.length(); i++) {
+            res += Integer.parseInt(String.valueOf(num.charAt(i)));
+        }
+        return res;
     }
 }
