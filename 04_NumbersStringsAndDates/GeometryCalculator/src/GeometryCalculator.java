cc76339d23471b901b13fc94bf2cfe487f2f9ef1
Strelkov Maxim <stremaks@icloud.com>
2020-07-05 18:51:45 +0300

    HomeWork 4.3

--- 04_NumbersStringsAndDates/GeometryCalculator/src/GeometryCalculator.java
+++ 04_NumbersStringsAndDates/GeometryCalculator/src/GeometryCalculator.java
@@ -1,21 +1,30 @@
+import static java.lang.Math.*;
+
 public class GeometryCalculator {
     // метод должен использовать абсолютное значение radius
     public static double getCircleSquare(double radius) {
+        if (radius > 0)
+            return PI * pow(radius, 2);
         return 0;
     }
 
     // метод должен использовать абсолютное значение radius
     public static double getSphereVolume(double radius) {
+        if (radius > 0)
+            return (4.0 / 3) * PI * pow(radius, 3);
         return 0;
     }
 
     public static boolean isTriangleRightAngled(double a, double b, double c) {
-        return false;
+        return (a + b) > c && (b + c) > a && (a + c) > b;
     }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTriangleRightAngled, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        if (isTriangleRightAngled(a, b, c))
+            return sqrt(p * (p - a) * (p - b) * (p - c));
         return 0;
     }
 }
