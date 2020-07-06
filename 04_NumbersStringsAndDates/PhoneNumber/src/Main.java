46dc78007b0b95ca5e92dd3174fabfd9dd5e6c42
Strelkov Maxim <stremaks@icloud.com>
2020-07-06 14:55:34 +0300

    HomeWork 4.5.4 done

--- 04_NumbersStringsAndDates/PhoneNumber/src/Main.java
+++ 04_NumbersStringsAndDates/PhoneNumber/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        StringBuilder telNumber = new StringBuilder(
+                new Scanner(System.in).nextLine().replaceAll("\\D", ""));
+
+        if (telNumber.length() > 11 || telNumber.length() < 10) {
+            System.out.println("Неверный формат номера");
+            return;
+        }
+        if (telNumber.length() == 10)
+            System.out.println('7' + telNumber.toString());
+        else {
+            if (telNumber.toString().charAt(0) == '7')
+                System.out.println(telNumber);
+            else if (telNumber.toString().charAt(0) == '8')
+                System.out.println(telNumber.toString().replaceFirst("8", "7"));
+            else
+                System.out.println("Неверный формат номера");
+        }
     }
 }
