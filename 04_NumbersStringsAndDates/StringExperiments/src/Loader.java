0b4dcac74891889ea7d6b8e3b69733a4e90a77ce
Strelkov Maxim <stremaks@icloud.com>
2020-07-07 11:12:13 +0300

    HomeWork 4.4.2 corrected

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -5,11 +5,9 @@ public class Loader {
     public static void main(String[] args) {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
         int sum = 0;
-        Pattern p = Pattern.compile("\\d+\\D");
-        Matcher m = p.matcher(text);
 
-        while (m.find())
-            sum += Integer.parseInt(text.substring(m.start(), m.end() - 1));
+        sum += Integer.parseInt(text.substring(text.indexOf("5"), text.indexOf("5") + 4));
+        sum += Integer.parseInt(text.substring(text.lastIndexOf("3"), text.lastIndexOf("3") + 5));
         System.out.println(sum);
     }
 }
