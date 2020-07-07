c7b117e61b4f543476ae0c5de9e5e77b3595a4b5
Strelkov Maxim <stremaks@icloud.com>
2020-07-07 23:04:44 +0300

    HomeWork 5.1 done

--- 05_ArraysAndCollections/Revers/src/Main.java
+++ 05_ArraysAndCollections/Revers/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        String[] text = {"Каждый", "охотник", "желает", "знать,", "где", "сидит", " фазан."};
 
+        for (int i = 0; i < text.length / 2; i++) {
+            String rev = "";
+            rev = text[i];
+            text[i] = text[text.length - i - 1];
+            text[text.length - i - 1] = rev;
+        }
+
+        for(String s : text)
+            System.out.print(s + " ");
     }
 }
