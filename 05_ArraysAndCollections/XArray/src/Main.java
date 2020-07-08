ea7dc7ab4336a31791efafe1833d43074086d9dd
Strelkov Maxim <stremaks@icloud.com>
2020-07-08 22:06:43 +0300

    HomeWork 5.* done

--- 05_ArraysAndCollections/XArray/src/Main.java
+++ 05_ArraysAndCollections/XArray/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        
+        char[][] arr = new char[7][7];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (i + j == arr[i].length - 1 || i == j)
+                    arr[i][j] = 'X';
+                else
+                    arr[i][j] = ' ';
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
