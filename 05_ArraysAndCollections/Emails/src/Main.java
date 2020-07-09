892e035127ab791f082de444caea793fceeb4941
Strelkov Maxim <stremaks@icloud.com>
2020-07-09 16:56:07 +0300

    HomeWork 5.3 done

--- 05_ArraysAndCollections/Emails/src/Main.java
+++ 05_ArraysAndCollections/Emails/src/Main.java
@@ -1,5 +1,34 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Main {
+    private static HashSet<String> emails = new HashSet<>();
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String cmd;
+
+        while (true) {
+            cmd = sc.nextLine();
+            if (cmd.matches("ADD\\s+.+"))
+                addEmail(cmd);
+            else if (cmd.equals("LIST"))
+                for (String email : emails)
+                    System.out.println(email);
+            else
+                System.out.println("Invalid command");
+        }
 
     }
+
+    private static void addEmail(String cmd) {
+        String email = cmd.split("\\s+")[1].trim();
+
+        if (email.matches(".+@.+\\..+"))
+            emails.add(email);
+        else
+            System.out.println("Invalid email");
+    }
+
 }
