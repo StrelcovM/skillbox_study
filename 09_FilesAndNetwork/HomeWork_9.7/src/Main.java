b5f06fa5c3ae2f399b4f09c1a7a6cf3fac58d6e3
Strelkov Maxim <stremaks@icloud.com>
2020-07-26 21:54:10 +0300

    HW 9.7 done

--- 09_FilesAndNetwork/HomeWork_9.7/src/Main.java
+++ 09_FilesAndNetwork/HomeWork_9.7/src/Main.java
@@ -1,5 +1,67 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.Scanner;
 
+public class Main {
+    private static String pathFrom;
+    private static String pathTo;
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        for (; ; ) {
+            System.out.println("Папка для копирования: ");
+            pathFrom = sc.nextLine();
+
+            File folderFrom = new File(pathFrom);
+
+            if (!folderFrom.exists() || folderFrom.list().length == 0) {
+                System.out.println("Папка пуста или не существует!");
+                continue;
+            }
+
+            System.out.println("Папка назначения: ");
+            pathTo = sc.nextLine();
+
+            File folderTo = new File(pathTo);
+
+            copyFolder(folderFrom, folderTo);
+
+        }
+    }
+
+    public static void copyFolder(File src, File dest)
+            throws IOException {
+
+        if (src.isDirectory()) {
+
+            if (!dest.exists()) {
+                dest.mkdir();
+            }
+
+            String[] files = src.list();
+
+            for (String file : files) {
+
+                File srcFile = new File(src, file);
+                File destFile = new File(dest, file);
+
+                copyFolder(srcFile, destFile);
+            }
+
+        } else {
+            InputStream in = new FileInputStream(src);
+            OutputStream out = new FileOutputStream(dest);
+
+            byte[] buffer = new byte[1024];
+
+            int length;
+
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+
+            in.close();
+            out.close();
+        }
     }
 }
