4fc1c45f33e25d98db50de0ad4714f9d86cdeaf6
Strelkov Maxim <stremaks@icloud.com>
2020-07-31 23:10:51 +0300

    HomeWork 9.9 done

---
+++ 09_FilesAndNetwork/HomeWork_9.9/src/model/Transaction.java
@@ -0,0 +1,78 @@
+package model;
+
+import java.math.BigInteger;
+
+public class Transaction {
+    private String accountType;
+    private BigInteger accountNumber;
+    private String accountCurrency;
+    private String operationDate;
+    private String transactionReference;
+    private String operationDescription;
+    private double income;
+    private double expenditure;
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public BigInteger getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(BigInteger accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountCurrency() {
+        return accountCurrency;
+    }
+
+    public void setAccountCurrency(String accountCurrency) {
+        this.accountCurrency = accountCurrency;
+    }
+
+    public String getOperationDate() {
+        return operationDate;
+    }
+
+    public void setOperationDate(String operationDate) {
+        this.operationDate = operationDate;
+    }
+
+    public String getTransactionReference() {
+        return transactionReference;
+    }
+
+    public void setTransactionReference(String transactionReference) {
+        this.transactionReference = transactionReference;
+    }
+
+    public String getOperationDescription() {
+        return operationDescription;
+    }
+
+    public void setOperationDescription(String operationDescription) {
+        this.operationDescription = operationDescription;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
+    public double getExpenditure() {
+        return expenditure;
+    }
+
+    public void setExpenditure(double expenditure) {
+        this.expenditure = expenditure;
+    }
+}
