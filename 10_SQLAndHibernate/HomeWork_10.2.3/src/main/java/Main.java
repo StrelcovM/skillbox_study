5693e336fc70b294b5b53cf5c2158f9126d3e816
Maxim Strelkov <stremaks@icloud.com>
2020-08-17 23:03:34 +0300

    HW_10.2.3 done

---
+++ 10_SQLAndHibernate/HomeWork_10.2.3/src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/skillbox?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        String user = "root";
+        String password = "ьщылщмлдшт";
+
+        Connection connection = DriverManager.getConnection(url, user, password);
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("select p.course_name, count(p.subscription_date) / (max(month(p.subscription_date)) - min(month(p.subscription_date)) + 1)\n" +
+                "from purchaselist p \n" +
+                "where year(p.subscription_date) = 2018\n" +
+                "group by p.course_name;");
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString(1) + " " + resultSet.getString(2));
+        }
+
+    }
+}
