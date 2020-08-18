4a96d8382eed7ef2f59edeb50ec44d29a8ce68bb
Maxim Strelkov <stremaks@icloud.com>
2020-08-18 14:33:12 +0300

    add try with resources. add query string

--- 10_SQLAndHibernate/HomeWork_10.2.3/src/main/java/Main.java
+++ 10_SQLAndHibernate/HomeWork_10.2.3/src/main/java/Main.java
@@ -1,26 +1,24 @@
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/skillbox?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
         String user = "root";
         String password = "ьщылщмлдшт";
-
-        Connection connection = DriverManager.getConnection(url, user, password);
-        Statement statement = connection.createStatement();
-
-        ResultSet resultSet = statement.executeQuery("select p.course_name, count(p.subscription_date) / (max(month(p.subscription_date)) - min(month(p.subscription_date)) + 1)\n" +
+        String query = "select p.course_name, count(p.subscription_date) / (max(month(p.subscription_date)) - min(month(p.subscription_date)) + 1)\n" +
                 "from purchaselist p \n" +
                 "where year(p.subscription_date) = 2018\n" +
-                "group by p.course_name;");
+                "group by p.course_name;";
 
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString(1) + " " + resultSet.getString(2));
+        try(Connection connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);) {
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-
-        resultSet.close();
-        statement.close();
-        connection.close();
-
     }
 }
