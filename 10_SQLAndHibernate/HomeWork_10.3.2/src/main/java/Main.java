4552e7d588220aff2ccc50043c8b4f4f5b4ba8f5
Maxim Strelkov <stremaks@icloud.com>
2020-08-19 12:03:51 +0300

    try-with-resources

--- 10_SQLAndHibernate/HomeWork_10.3.2/src/main/java/Main.java
+++ 10_SQLAndHibernate/HomeWork_10.3.2/src/main/java/Main.java
@@ -1,6 +1,5 @@
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -9,22 +8,22 @@ import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) {
-        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+        try (StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure("hibernate.cfg.xml")
                 .build();
-        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
-        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
-        Session session = sessionFactory.openSession();
+             SessionFactory sessionFactory = new MetadataSources(registry).getMetadataBuilder().build().getSessionFactoryBuilder().build();
+             Session session = sessionFactory.openSession()) {
 
-        BigInteger countCourses = (BigInteger) session
-                .createSQLQuery("SELECT COUNT(*) FROM skillbox.courses")
-                .uniqueResult();
+            BigInteger countCourses = (BigInteger) session
+                    .createSQLQuery("SELECT COUNT(*) FROM skillbox.courses")
+                    .uniqueResult();
 
-        int count = countCourses.intValue();
+            int count = countCourses.intValue();
 
-        for(int i = 1; i < count; i++) {
-            Course course = session.get(Course.class, i);
-            System.out.println(course.getName() + " " + course.getStudentsCount());
+            for (int i = 1; i < count; i++) {
+                Course course = session.get(Course.class, i);
+                System.out.println(course.getName() + " " + course.getStudentsCount());
+            }
         }
     }
 }
