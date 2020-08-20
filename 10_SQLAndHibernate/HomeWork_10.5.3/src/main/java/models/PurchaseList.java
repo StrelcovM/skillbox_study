6d908da7a0faf3734764b182fa01bb35b78abf88
Maxim Strelkov <stremaks@icloud.com>
2020-08-21 00:04:43 +0300

    purchaseList added. HW done

--- 10_SQLAndHibernate/HomeWork_10.5.3/src/main/java/models/PurchaseList.java
+++ 10_SQLAndHibernate/HomeWork_10.5.3/src/main/java/models/PurchaseList.java
@@ -1,5 +1,87 @@
 package models;
 
-public class PurchaseList {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
 
+@Entity
+@Table(name = "purchaselist")
+public class PurchaseList {
+    @EmbeddedId
+    private PurchaseKey key;
+
+    @Column(name = "student_name", insertable = false, updatable = false)
+    private String studentName;
+    @Column(name = "course_name", insertable = false, updatable = false)
+    private String courseName;
+
+    private int price;
+
+    @Column(name = "subscription_date")
+    private Date subscriptionDate;
+
+    @Embeddable
+    public static class PurchaseKey implements Serializable {
+        @Column(name = "student_name")
+        private String student;
+
+        @Column(name = "course_name")
+        private String CourseName;
+
+        public PurchaseKey() {
+        }
+
+        public PurchaseKey(String student, String courseName) {
+            this.student = student;
+            CourseName = courseName;
+        }
+
+        public String getStudent() {
+            return student;
+        }
+
+        public void setStudent(String student) {
+            this.student = student;
+        }
+
+        public String getCourseName() {
+            return CourseName;
+        }
+
+        public void setCourseName(String courseName) {
+            CourseName = courseName;
+        }
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Date getSubscriptionDate() {
+        return subscriptionDate;
+    }
+
+    public void setSubscriptionDate(Date subscriptionDate) {
+        this.subscriptionDate = subscriptionDate;
+    }
 }
