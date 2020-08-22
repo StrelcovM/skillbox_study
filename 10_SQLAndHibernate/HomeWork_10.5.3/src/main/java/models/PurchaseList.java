3338de5ca9a5595c032c66c0955e81b9300824c2
Maxim Strelkov <stremaks@icloud.com>
2020-08-22 10:38:34 +0300

    10.5.3

--- 10_SQLAndHibernate/HomeWork_10.5.3/src/main/java/models/PurchaseList.java
+++ 10_SQLAndHibernate/HomeWork_10.5.3/src/main/java/models/PurchaseList.java
@@ -51,22 +51,6 @@ public class PurchaseList {
         }
     }
 
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
     public int getPrice() {
         return price;
     }
