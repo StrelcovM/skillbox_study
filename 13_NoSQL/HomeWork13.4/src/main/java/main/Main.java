116399c9d9c18456355e68434a0b67712c24b746
Maxim Strelkov <stremaks@icloud.com>
2020-10-08 15:38:43 +0300

    add avh product price and products count

--- 13_NoSQL/HomeWork13.4/src/main/java/main/Main.java
+++ 13_NoSQL/HomeWork13.4/src/main/java/main/Main.java
@@ -4,33 +4,39 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.UpdateOptions;
+import org.bson.BsonDocument;
 import org.bson.Document;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 import java.util.function.Consumer;
 
 public class Main {
     private static final MongoDatabase MONGO_DATABASE =
-            new MongoClient("127.0.0.1", 27017).getDatabase("local");
+            new MongoClient("127.0.0.1", 27017)
+                    .getDatabase("local");
     private static final MongoCollection<Document> PRODUCTS = MONGO_DATABASE.getCollection("products");
     private static final MongoCollection<Document> SHOPS = MONGO_DATABASE.getCollection("shops");
 
     static {
         PRODUCTS.drop();
         SHOPS.drop();
-        SHOPS.insertOne(new Document(Map.of("name", "магазин_1", "prod", Collections.emptyList())));
-        PRODUCTS.insertOne(new Document(Map.of("name", "вафли", "price", "50")));
+        SHOPS.insertOne(new Document(Map.of("name", "магазин_1", "prod", Collections.singletonList("молоко"))));
+        SHOPS.insertOne(new Document(Map.of("name", "магазин_2", "prod", Arrays.asList("хлеб", "молоко"))));
+        SHOPS.insertOne(new Document(Map.of("name", "магазин_3", "prod", Arrays.asList("вафли", "хлеб", "молоко"))));
+        PRODUCTS.insertOne(new Document(Map.of("name", "вафли", "price", 50)));
+        PRODUCTS.insertOne(new Document(Map.of("name", "молоко", "price", 20)));
+        PRODUCTS.insertOne(new Document(Map.of("name", "хлеб", "price", 10)));
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         while (true) {
-            SHOPS.find().forEach((Consumer<Document>) System.out::println);
-            PRODUCTS.find().forEach((Consumer<Document>) System.out::println);
             var cmd = reader.readLine();
             if (cmd.startsWith("ДОБАВИТЬ_ТОВАР"))
                 addProduct(cmd);
@@ -38,8 +44,8 @@ public class Main {
                 addShop(cmd);
             if (cmd.startsWith("ВЫСТАВИТЬ_ТОВАР"))
                 addProductInShop(cmd);
-            if (cmd.startsWith("СТАТИСТИКА_ТОВАРОВ"))
-                statistic();
+            if (cmd.startsWith("1"))
+                System.out.println(statistic());
         }
     }
 
@@ -76,6 +82,24 @@ public class Main {
     }
 
     private static String statistic() {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        BsonDocument query2 = BsonDocument
+                .parse("{$lookup: {from: \"products\"," +
+                        "localField: \"prod\"," +
+                        "foreignField: \"name\"," +
+                        " as: \"prods\"}}");
+
+        BsonDocument query = BsonDocument.parse("{ $unwind: \"$prods\"}");
+        BsonDocument query1 = BsonDocument
+                .parse("{ $group: {_id: \"$name\"," +
+                        "products_count: {$sum :1}," +
+                        "avg_products_price: {$avg:\"$prods.price\"}}}");
+
+        SHOPS.aggregate(Arrays.asList(query2, query, query1)).forEach((Consumer<Document>) doc -> {
+            builder.append(doc);
+            builder.append("\n\n");
+        });
+
+        return builder.toString();
     }
 }
