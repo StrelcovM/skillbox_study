4d64fd24c94b331f61e771a2eac42782ec7f29ab
Maxim Strelkov <stremaks@icloud.com>
2020-10-17 17:46:39 +0300

    add Multithreading

--- 14_Performance/CarNumberGenerator/src/Loader.java
+++ 14_Performance/CarNumberGenerator/src/Loader.java
@@ -1,42 +1,15 @@
-import java.io.PrintWriter;
-
 public class Loader {
     public static void main(String[] args) throws Exception {
-        int regionCode = 199;
-
         long start = System.currentTimeMillis();
+        Writer thread_1 = new Writer(199, "14_Performance/CarNumberGenerator/res/numbers_1.txt");
+        Writer thread_2 = new Writer(199, "14_Performance/CarNumberGenerator/res/numbers_2.txt");
 
-        PrintWriter writer = new PrintWriter("14_Performance/CarNumberGenerator/res/numbers.txt");
+        thread_1.start();
+        thread_2.start();
 
-        char[] letters = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
-        for (int number = 1; number < 1000; number++) {
-            StringBuilder builder = new StringBuilder();
-            for (char firstLetter : letters) {
-                for (char secondLetter : letters) {
-                    for (char thirdLetter : letters) {
-                        builder.append(firstLetter)
-                                .append(padNumber(number, 3))
-                                .append(secondLetter)
-                                .append(thirdLetter).append(padNumber(regionCode, 2))
-                                .append("\n");
-                    }
-                }
-            }
-            writer.write(builder.toString());
-        }
-
-        writer.flush();
-        writer.close();
+        thread_1.join();
+        thread_2.join();
 
         System.out.println((System.currentTimeMillis() - start) + " ms");
-    }
-
-    private static String padNumber(int number, int numberLength) {
-        StringBuilder numberStr = new StringBuilder(Integer.toString(number));
-        int padSize = numberLength - numberStr.length();
-        for (int i = 0; i < padSize; i++) {
-            numberStr.insert(0, '0');
-        }
-        return numberStr.toString();
     }
 }
